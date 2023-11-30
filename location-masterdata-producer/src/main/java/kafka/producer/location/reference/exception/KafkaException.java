cb2fa752e4bed44e8afcce686286057786b0ecef
agrawalharshit701 <148426348+agrawalharshit701@users.noreply.github.com>
2023-11-30 12:43:56 +0530

    Delete location-masterdata-producer directory

--- location-masterdata-producer/src/main/java/kafka/producer/location/reference/exception/KafkaException.java
+++
@@ -1,7 +0,0 @@
-package kafka.producer.location.reference.exception;
-
-public class KafkaException extends RuntimeException {
-    public KafkaException(String message) {
-        super(message);
-    }
-}
